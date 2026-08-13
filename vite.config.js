0020cb920af5f33ae7b9ab34cbbd134e7859d503
Thiago Graciano <msgraciano28@gmail.com>
2026-08-13 00:25:25 -0300

    Feat: Adiciona NavBar

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
 import { resolve, dirname } from 'path'
 import { fileURLToPath } from 'url'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "#components":
