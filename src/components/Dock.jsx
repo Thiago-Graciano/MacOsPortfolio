d23661dee8174a2bcbf9a4af6954340da3d8ab03
Thiago Graciano <msgraciano28@gmail.com>
2026-08-13 13:57:34 -0300

    Feat: Adiciona window store com a lógico de abrir, fechar e focus

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -4,8 +4,10 @@ import gsap from "gsap";
 
 import { dockApps } from "#constants/index.js";
 import { useGSAP } from "@gsap/react"
+import useWindowStore from "#store/window.js";
 
 const Dock = () => {
+    const { openWindow, closeWindow, windows } = useWindowStore() 
     const dockRef = useRef(null);
 
     useGSAP(() => {
@@ -58,7 +60,24 @@ const Dock = () => {
         }
     }, [])
 
-    const toggleApp = (aap) => { }
+    const toggleApp = (app) => {
+        if(!app.canOpen) return;
+
+        const window = windows[app.id];
+
+        if(!window) {
+            console.error(`Window not found in the store: ${app.id}`);
+            return;
+        }
+
+        if(window.isOpen) {
+            closeWindow(app.id)
+        } else {
+            openWindow(app.id)
+        }
+
+        console.log(windows)
+    }
 
     return (
         <section id="dock">
