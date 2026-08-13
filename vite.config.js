6dfd0bce5c3fad5fb39a6fddbf8905591b816a35
Thiago Graciano <msgraciano28@gmail.com>
2026-08-12 23:34:28 -0300

    Feat: Adiciona jsconfig e resolve em vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,28 @@ import { fileURLToPath } from 'url'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "#components":
+        resolve(dirname(fileURLToPath(import.meta.url)),
+          "src/components"
+        ),
+      "#constants":
+        resolve(dirname(fileURLToPath(import.meta.url)),
+          "src/constants"
+        ),
+      "#store":
+        resolve(dirname(fileURLToPath(import.meta.url)),
+          "src/store"
+        ),
+      "#hoc":
+        resolve(dirname(fileURLToPath(import.meta.url)),
+          "src/hoc"
+        ),
+      "#windows":
+        resolve(dirname(fileURLToPath(import.meta.url)),
+          "src/windows"
+        ),
+    }
+  }
 })
