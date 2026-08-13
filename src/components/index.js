a5f6d4c355cae013cc8f8d1939f8f5c0166e004a
Thiago Graciano <msgraciano28@gmail.com>
2026-08-13 15:09:37 -0300

    Feat: Adiciona terminal com portifólio

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import NavBar from "#components/NavBar.jsx"
 import Welcome from "#components/Welcome.jsx"
 import Dock from "#components/Dock.jsx"
+import WindowWrapper from "#hoc/WindowWrapper.jsx"
 
 export { NavBar, Welcome, Dock };
---
+++ src/hoc/WindowWrapper.jsx
@@ -0,0 +1,41 @@
+import { useRef, useLayoutEffect } from "react";
+import useWindowStore from "#store/window.js";
+import { useGSAP } from "@gsap/react"
+
+const WindowWrapper = (Component, windowKey) => {
+    const Wrapped = (props) => {
+        const { focusWindow, windows } = useWindowStore();
+        const { isOpen, zIndex } = windows[windowKey];
+        const ref = useRef(null);
+
+        useGSAP(() => {
+            const el = ref.current;
+            if(!el || !isOpen) return;
+
+            el.style.display = "block";
+        }, [isOpen])
+
+        useLayoutEffect(() => {
+            const el = ref.current;
+            if(!el) return;
+            el.style.display = isOpen ? "block" : "none";
+        }, [isOpen])
+
+        return (
+            <section
+                id={windowKey}
+                ref={ref}
+                style={{ zIndex }}
+                className="absolute"
+            >
+                <Component {...props} />
+            </section>
+        )
+    }
+
+    Wrapped.displayName = "WindowWrapper(${Component.displayName || Component.name || 'Component'})";
+
+    return Wrapped;
+}
+
+export default WindowWrapper
