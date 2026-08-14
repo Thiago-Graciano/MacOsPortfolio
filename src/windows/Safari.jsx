7ee87c201a19971543235334db92837d00fa3abd
Thiago Graciano <msgraciano28@gmail.com>
2026-08-13 23:22:12 -0300

    Feat: Adiciona Safari Window

---
+++ src/windows/Safari.jsx
@@ -0,0 +1,76 @@
+import WindowControls from "#components/WindowControls.jsx";
+import WindowWrapper from "#hoc/WindowWrapper.jsx";
+import {
+    ShieldHalf,
+    ChevronLeft,
+    ChevronRight,
+    PanelLeft,
+    Search,
+    Share,
+    Plus,
+    Copy,
+    MoveRight,
+} from "lucide-react";
+import { blogPosts } from "#constants";
+
+const Safari = () => {
+    return (
+        <>
+            <div id="window-header">
+                <WindowControls target="safari" />
+
+                <PanelLeft className="ml-10 icon" />
+
+                <dvi className="flex items-center gap-1 ml-5">
+                    <ChevronLeft className="icon" />
+                    <ChevronRight className="icon" />
+                </dvi>
+
+                <div className="flex-1 flex-center gap-3">
+                    <ShieldHalf className="icon" />
+
+                    <div className="search">
+                        <Search className="icon" />
+
+                        <input
+                            type="text"
+                            placeholder="Search or enter website name"
+                            className="flex-1"
+                        />
+                    </div>
+                </div>
+                <div className="flex items-center gap-5">
+                    <Share className="icon" />
+                    <Plus className="icon" />
+                    <Copy className="icon" />
+                </div>
+            </div>
+
+            <div className="blog">
+                <h2>Meus posts no LinkedIn</h2>
+
+                <div className="space-y-8">
+                    {blogPosts.map(({ id, image, title, date, link }) => (
+                        <div key={id} className="blog-post">
+                            <div className="col-span-2">
+                                <img src={image} alt={title} />
+                            </div>
+
+                            <div className="content">
+                                <p>{date}</p>
+                                <h3>{title}</h3>
+                                <a href={link} target="_blank" rel="noopener noreferrer">
+                                    Clique para ver o Post! <MoveRight className="icon-hover"/>
+                                </a>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+}
+
+const SafariWindow = WindowWrapper(Safari, "safari");
+
+export default SafariWindow;
